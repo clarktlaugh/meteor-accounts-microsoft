0efbe1a50a776f219a88557379c24b49130457d6
Clark Laughlin <clark@laughlins.org>
2016-06-22 12:17:13 -0500

    onedrive -> microsoft

--- onedrive_client.js
+++ onedrive_client.js
@@ -1,18 +1,18 @@
-OneDrive = {};
+Microsoft = {};
 
-// Request OneDrive credentials for the user
+// Request Microsoft credentials for the user
 // @param options {optional}
 // @param credentialRequestCompleteCallback {Function} Callback function to call on
 //   completion. Takes one argument, credentialToken on success, or Error on
 //   error.
-OneDrive.requestCredential = function (options, credentialRequestCompleteCallback) {
+Microsoft.requestCredential = function (options, credentialRequestCompleteCallback) {
   // support both (options, callback) and (callback).
   if (!credentialRequestCompleteCallback && typeof options === 'function') {
     credentialRequestCompleteCallback = options;
     options = {};
   }
 
-  var config = ServiceConfiguration.configurations.findOne({service: 'onedrive'});
+  var config = ServiceConfiguration.configurations.findOne({service: 'microsoft'});
   if (!config) {
     credentialRequestCompleteCallback && credentialRequestCompleteCallback(
       new ServiceConfiguration.ConfigError());
@@ -23,17 +23,17 @@ OneDrive.requestCredential = function (options, credentialRequestCompleteCallbac
   var scope = (options && options.requestPermissions) || ["wl.basic","wl.skydrive","wl.emails","wl.offline_access"];
   var flatScope = _.map(scope, encodeURIComponent).join('+');
 
-  var loginStyle = OAuth._loginStyle('onedrive', config, options);
+  var loginStyle = OAuth._loginStyle('microsoft', config, options);
 
   var loginUrl =
     'https://login.live.com/oauth20_authorize.srf' +
     '?client_id=' + config.clientId +
     '&scope=' + flatScope +
     '&response_type=code' +
-    '&redirect_uri=' + encodeURI(OAuth._redirectUri('onedrive', config)) +
+    '&redirect_uri=' + encodeURI(OAuth._redirectUri('microsoft', config)) +
     '&state=' + OAuth._stateParam(loginStyle, credentialToken);
   OAuth.launchLogin({
-    loginService: "onedrive",
+    loginService: "microsoft",
     loginStyle: loginStyle,
     loginUrl: loginUrl,
     credentialRequestCompleteCallback: credentialRequestCompleteCallback,
