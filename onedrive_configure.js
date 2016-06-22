affda93068d1ebc596b08001b5d00ba28c91b030
Clark Laughlin <clark@laughlins.org>
2016-06-22 12:17:45 -0500

    onedrive -> microsoft

--- onedrive_configure.js
+++ onedrive_configure.js
@@ -1,8 +1,8 @@
-Template.configureLoginServiceDialogForOnedrive.siteUrl = function () {
+Template.configureLoginServiceDialogForMicrosoft.siteUrl = function () {
   return Meteor.absoluteUrl();
 };
 
-Template.configureLoginServiceDialogForOnedrive.fields = function () {
+Template.configureLoginServiceDialogForMicrosoft.fields = function () {
   return [
     {property: 'clientId', label: 'Client ID'},
     {property: 'secret', label: 'Client Secret'}
